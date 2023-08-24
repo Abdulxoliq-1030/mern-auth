ade2b75b53fbedef1fd29648fa3c78f755bc8e6d
Abdulxoliq Odiljonov <abdulxoliqodiljonov@Abdulxoliqs-MacBook-Pro.local>
2023-08-24 18:22:36 +0500

    app complete

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -10,9 +10,11 @@ import {
 import store from "./store.js";
 import { Provider } from "react-redux";
 import "bootstrap/dist/css/bootstrap.min.css";
+import PrivateRoute from "./components/PrivateRoute.jsx";
 import HomeScreen from "./screens/HomeScreen.jsx";
 import LoginScreen from "./screens/LoginScreen.jsx";
 import RegisterScreen from "./screens/RegisterScreen.jsx";
+import ProfileScreen from "./screens/ProfileScreen.jsx";
 import "./index.css";
 
 const router = createBrowserRouter(
@@ -21,6 +23,10 @@ const router = createBrowserRouter(
       <Route index={true} path="/" element={<HomeScreen />} />
       <Route path="/login" element={<LoginScreen />} />
       <Route path="/register" element={<RegisterScreen />} />
+      {/* Private routes */}
+      <Route path="" element={<PrivateRoute />}>
+        <Route path="/profile" element={<ProfileScreen />} />
+      </Route>
     </Route>
   )
 );
